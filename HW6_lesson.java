db03ff580a169206311ee8a5f47ee81c31b11461
ljohnpetrov <hant1595@gmail.com>
2017-12-10 23:37:32 +0300

    Исправлены ошибки и добавлен полиморфизм

--- HW6_lesson.java
+++ HW6_lesson.java
@@ -1,31 +1,91 @@
-public class HW6_lesson {
+/**
+ * Java. Level 1. Lesson 6. Homework
+ *
+ * @author Ivan Petrov
+ * @version dated Nov 30, 2017
+ * @link 
+ */
+public class Main {
 
     public static void main(String[] args) {
-        Cat cat1 = new Cat(run);
+        Animal[] animal = {new Cat(), new Dog()};
+        Cat cat1 = new Cat();
         Dog dog1 = new Dog();
-        System.out.println(cat1.run(150))
-        
+        for (Animal beast : animal) {
+            System.out.println(beast.name() + ".run(200) -> rezult: " + beast.run(150));
+            System.out.println(beast.name() + ".jump(0.3) -> rezult: " + beast.jump(0.3));
+            System.out.println(beast.name() + ".swim(2) -> rezult: " + beast.swim(2));
+            System.out.println();
+        }
     }
 }
-class Animal {
-    int run;
-    int jump;
-    int swim;
-    boolean run() {
-        if (run < 200)
-        return true;
-    return false;
-        
+interface IAnimal {
+    boolean run(int r);
+    boolean swim(int s);
+    boolean jump(double j);
+    String name();
+}
+
+abstract class Animal implements IAnimal {
+    String name;
+    int r;
+    int s;
+    double j;
+    Animal(){
+        this.name = name;
+        this.r = r;
+        this.s = s;
+        this.j = j;
     }
-   // swim() {
-   // }
-   // jump() {
-   // }
-    
 }
-public class Cat extends Animal{
 
-}
-public class Dog extends Animal{
-
+class Cat extends Animal {
+    Cat() {
+        this.name = name;
+        this.r = r;
+        this.s = s;
+        this.j = j;
+    }
+    public String name(){
+        return "cat";
+    }
+    public boolean run(int r) {
+        if (r <= 200)
+            return true;
+        return false;
+        }
+    public boolean swim(int s) {
+        return false;
+    }
+    public boolean jump(double j) {
+        if (j <= 2)
+            return true;
+        return false;
+        }
+    }
+class Dog extends Animal {
+    Dog() {
+        this.name = name;
+        this.r = r;
+        this.s = s;
+        this.j = j;
+    }
+    public String name(){
+        return "dog";
+    }
+    public boolean run(int r) {
+        if (r <= 500)
+            return true;
+        return false;
+    }
+    public boolean swim(int s) {
+        if (s <= 10)
+            return true;
+        return false;
+    }
+    public boolean jump(double j) {
+        if (j <= 0.5)
+            return true;
+        return false;
+    }
 }
