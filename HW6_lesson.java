a32c67f925e130633aa2cd2e62e57f4bc042e093
ljohnpetrov <hant1595@gmail.com>
2017-12-10 21:25:57 +0300

    update homework

--- HW6_lesson.java
+++ HW6_lesson.java
@@ -1,3 +1,31 @@
 public class HW6_lesson {
 
     public static void main(String[] args) {
+        Cat cat1 = new Cat(run);
+        Dog dog1 = new Dog();
+        System.out.println(cat1.run(150))
+        
+    }
+}
+class Animal {
+    int run;
+    int jump;
+    int swim;
+    boolean run() {
+        if (run < 200)
+        return true;
+    return false;
+        
+    }
+   // swim() {
+   // }
+   // jump() {
+   // }
+    
+}
+public class Cat extends Animal{
+
+}
+public class Dog extends Animal{
+
+}
